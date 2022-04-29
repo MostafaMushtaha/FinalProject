1928b9e263483a3822828d5dd74347cc121be240
TarStyx <83635799+MostafaMushtaha@users.noreply.github.com>
2022-04-30 00:27:47 +0200

    Pages
    Added some of the pages

--- src/app/layout/layout-container/layout-routing.module.ts
+++ src/app/layout/layout-container/layout-routing.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 import { HomeComponent } from 'src/app/pages/home/home.component';
+import { Home2Component } from 'src/app/pages/home2/home2.component';
 import { AboutUsComponent } from 'src/app/pages/about-us/about-us.component';
 import { ContactUsComponent } from 'src/app/pages/contact-us/contact-us.component';
+import { SignUpComponent } from 'src/app/pages/sign-up/sign-up.component';
+import { LoginComponent } from 'src/app/pages/login/login.component';
+import { VideoCallComponent } from 'src/app/pages/video-call/video-call.component';
 
 const routes: Routes = [
   {
     path: '', component: LayoutComponent, children: [
       { path: 'home', component: HomeComponent },
+      { path: 'home2', component: Home2Component },
       { path: 'about-us', component: AboutUsComponent },
       { path: 'contact-us', component: ContactUsComponent },
-      { path: '', redirectTo: 'about-us', pathMatch: 'full' }
+      { path: 'sign-up', component: SignUpComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'video-call', component: VideoCallComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' }
     ],
   }
 ];
